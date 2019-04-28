ca41ffde28827a5b5589a65e10d3e07f7412c51c
Logan Lembke <logan.lembke@mines.sdsmt.edu>
2019-04-27 18:43:22 -0600

    Implement distance_matrix_vector/ all_pairs_distance_matrix

--- LossHardNet.py
+++ LossHardNet.py
@@ -1,3 +1,6 @@
+import torch
+
+
 class LossHardNet:
     def __init__(self, margin):
         """
@@ -15,29 +18,46 @@ class LossHardNet:
         """
         pass
 
-    def _get_negative(self, anchor, positive):
+    @staticmethod
+    def _get_negative(anchor, positive):
         """
         type: (LossHardNet, Variable, Variable)-> Variable
         TODO: finish commenting
-        :param anchor:
-        :param positive:
+        :param anchor: NXD matrix of N D dimensional descriptors
+        :param positive: NXD matrix of N D dimensional descriptors
         :return:
         """
-        pass
 
-    def _distance_matrix_vector(self, positive, anchor):
-        """
-        type: (LossHardNet, Variable, Variable)-> Variable
-        TODO: finish commenting
-        TODO: get a better name
-        :param positive:
-        :param anchor:
-        :return:
-        """
+        assert anchor.size() == positive.size()
+        assert anchor.dim() == 2
+
+
+        distance_matrix =  LossHardNet.__all_pairs_distance_matrix(anchor, positive) # + eps?
+
         pass
 
     @staticmethod
-    def _filter_distance_matrix(distances):
+    def __all_pairs_distance_matrix(anchor, positive):
+        """
+        type: (LossHardNet, Variable, Variable)-> Variable
+
+        Calculates the pairwise distance between each row vector in anchor and each row vector
+        in positive. Stores the distances in a symmetric matrix.
+
+        :param anchor: NXD matrix of N D dimensional descriptors
+        :param positive: NXD matrix of N D dimensional descriptors
+        :return: NXN symmetric matrix of pairwise distance calculations
+        """
+
+        # Use the fact that ||x1 - x2||^2 = ||x1||^2 + ||x2||^2 - 2*<x1,x2>
+
+        anchor_norm_sq = torch.sum(anchor * anchor, dim=(1,)).unsqueeze(-1).repeat((1, anchor.size(0)))
+        positive_norm_sq = torch.t(torch.sum(anchor * anchor, dim=(1,)).unsqueeze(-1).repeat((1, anchor.size(0))))
+
+        return torch.sqrt(anchor_norm_sq + positive_norm_sq - 2.0 * torch.mm(anchor, torch.t(positive)))
+
+    @staticmethod
+    def __filter_distance_matrix(distances):
         """
         type: (Variable)->Variable
         TODO: commenting and implementation
